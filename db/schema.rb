63876cdd05e84dcd220798311433435488b84ea6
michel349u <michel349u@etu.univ-lorraine.fr>
2024-04-25 12:10:00 +0200

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_075332) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_083801) do
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "doctor_id", null: false
     t.integer "patient_id", null: false
+    t.integer "city_id", null: false
+    t.index ["city_id"], name: "index_appointments_on_city_id"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.string "zip_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -28,6 +37,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_075332) do
     t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id", null: false
+    t.index ["city_id"], name: "index_doctors_on_city_id"
+  end
+
+  create_table "doctors_specialties", id: false, force: :cascade do |t|
+    t.integer "doctor_id", null: false
+    t.integer "specialty_id", null: false
   end
 
   create_table "patients", force: :cascade do |t|
@@ -35,8 +51,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_075332) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id", null: false
+    t.index ["city_id"], name: "index_patients_on_city_id"
   end
 
+  create_table "specialties", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "appointments", "cities"
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "patients"
+  add_foreign_key "doctors", "cities"
+  add_foreign_key "patients", "cities"
 end
